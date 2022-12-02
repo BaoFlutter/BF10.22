6991203d8195c3cda68b931314cfa80622e1d466
BaoFlutter <baoflutter@gmail.com>
2022-12-02 21:14:38 +0700

    add provider

---
+++ provider_lession/lib/data_sources/api_service.dart
@@ -0,0 +1,35 @@
+// Xử lý các nhiệm vụ của các API
+import 'package:api_lession/data_sources/api_url.dart';
+import 'package:http/http.dart' as http;
+
+class ApiService{
+
+ // Đăng nhập
+
+  Future signIn({required Map loginData})
+  {
+    var loginUrl = ApiUrl.loginApiUrl;
+    Future response = http.post(
+        loginUrl,
+        body: loginData);
+    return response;
+  }
+
+
+  Future logOut( token){
+    var logOut = ApiUrl.lougOutApiUrl;
+    Future response = http.delete(logOut,
+        headers:{
+          "Authorization" : 'Bearer ' + token,
+          "Accept" : "application/json"
+        });
+    return response;
+
+  }
+
+
+
+
+
+
+}
---
+++ provider_lession/lib/providers/user_provider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import '../models/user.dart';
+
+class UserProvider with ChangeNotifier {
+  User? user ;
+
+  updateUserInformation({ required User user}){
+    this.user = user;
+    notifyListeners();
+  }
+
+
+
+}
